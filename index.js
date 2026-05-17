623e80cd8fb9a12cd0d1971dbc3f89e249bdcab7
CSROREONLINE <jjamesmccloudd@gmail.com>
2026-05-17 10:50:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const update1Button = document.getElementById('update1Button');
 update1Button.addEventListener('click', () => {
     window.open('https://www.mediafire.com/file/6n5oeciri99gy51/CounterStrike_-_ReOnline_v1.1.3_-_BugFix.pkg/file'), ('_blank');
 });
+
+function filtrarVaina () {
+    update1Button.style.display = 'none';
+};
